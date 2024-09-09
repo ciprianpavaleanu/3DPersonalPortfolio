3f6fd9d646bee4a9e266d5954e8885f0e30e2022
Pavaleanu Ciprian <pavaleanuciprian@yahoo.com>
2024-09-09 21:11:46 +0300

    deploy problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import { Home, About, Projects, Contact } from "./pages";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
       <Router>
-        <NavBar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
